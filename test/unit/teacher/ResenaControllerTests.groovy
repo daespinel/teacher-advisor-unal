2e07509bfc2c05ad11bd848920451fcd1d04651d
olpomarehe <olpomarehe@unal.edu.co>
2014-04-18 19:36:26 -0500

    Rese?a con scaffolding dinamico

---
+++ test/unit/teacher/ResenaControllerTests.groovy
@@ -0,0 +1,17 @@
+package teacher
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(ResenaController)
+class ResenaControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
