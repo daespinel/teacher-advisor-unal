853fa14022362e40b5ba470d897fceac81a85dad
Camilo Delgado <Camilo Delgado@localhost>
2014-06-13 00:32:37 -0500

    -Cambiadas restrcciones del campo Caracteristica en Servicio y Producto -Mejorado y Arreglado el Filtro de Palabras

--- grails-app/services/teacher/VerificarLenguajeService.groovy
+++ grails-app/services/teacher/VerificarLenguajeService.groovy
@@ -5,26 +5,58 @@ import grails.transaction.Transactional
 @Transactional
 class VerificarLenguajeService {
 
-    def buscarProfanidades(String texto) {
+	def buscarProfanidades(String texto) {
 		if (!texto) return false
-		
-		List profan = [/(?i)(\s)?f(\s)?.+c(\s)?k*(\s)?(.+n(g)?)?(\s)?/,
-			/(?i)(\s)?s(\s)?h(\s)?.+(\s)?t(\s)?/,
-			/(?i)(\s)?bullsh.+t(\s)?/,
-			/(?i)(\s)?hijo(\s)?de(\s)?puta(\s)?/]
-		
+
+		List profan = [
+			/(?i)(\s)?f.ck(\s)?/,
+			/(?i)(\s)?shit(\s)?/,
+			/(?i)(\s)?hijo(\s)?de(\s)?puta(\s)?/,
+			/(?i)(\s)?marica(\s)?/,
+			/(?i)(\s)?mierda(\s)?/,
+			/(?i)(\s)?puto(\s)?/,
+			/(?i)(\s)?malparido(\s)?/,
+			/(?i)(\s)?maricon(\s)?/,
+			/(?i)(\s)?monda(\s)?/,
+			/(?i)(\s)?mamahuevo(\s)?/,
+			/(?i)(\s)?huevo(\s)?/,
+			/(?i)(\s)?chimba(\s)?/,
+			/(?i)(\s)?pene(\s)?/,
+			/(?i)(\s)?vagina(\s)?/,
+			/(?i)(\s)?culiao(\s)?/,
+			/(?i)(\s)?pija(\s)?/,
+			/(?i)(\s)?pito(\s)?/,
+			/(?i)(\s)?puta(\s)?/,
+			/(?i)(\s)?noob(\s)?/,
+			/(?i)(\s)?gonorrea(\s)?/,
+			/(?i)(\s)?pirobo(\s)?/,
+			/(?i)(\s)?basura(\s)?/,
+			/(?i)(\s)?pichurria(\s)?/,
+			/(?i)(\s)?culo(\s)?/,
+			/(?i)(\s)?verga(\s)?/,
+			/(?i)(\s)?guevon(\s)?/,
+			/(?i)(\s)?percanta(\s)?/,
+			/(?i)(\s)?zunga(\s)?/,
+			/(?i)(\s)?fufa(\s)?/,
+			/(?i)(\s)?retrasado(\s)?/,
+			/(?i)(\s)?pajazo(\s)?/,
+			/(?i)(\s)?gurrupleta(\s)?/,
+		]
+
 		List pMatch = []
-		
+
 		for (i in profan){
-			
-			texto = texto.replaceAll(i,"**beep**")
-			
-			//pMatch = texto.findAll(i)
+			println i
+			pMatch = texto.findAll(i)
+			println pMatch
+			texto = texto.replaceAll(i,"****")
+
+
 			//if (!pMatch.empty){
-				//break
+			//break
 			//}
 		}
 		//return pMatch.empty
 		return texto
-    }
+	}
 }
